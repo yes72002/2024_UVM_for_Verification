593094b2eb217568e3ad55af703840903622a232
yes72002 <yes72002@gmail.com>
2024-11-02 00:21:41 +0800

    add baudrate and fix <=

--- part2_class4_uart/u2_uart_tx.sv
+++ part2_class4_uart/u2_uart_tx.sv
@@ -41,6 +41,14 @@ module clk_gen(
           rx_max <= 11'd54;
           tx_max <= 14'd868;
         end
+        115200: begin
+          rx_max <= 11'd27;
+          tx_max <= 14'd434;
+        end
+        128000: begin
+          rx_max <= 11'd24;
+          tx_max <= 14'd392;
+        end
         default: begin
           rx_max <= 11'd325;
           tx_max <= 14'd5208;
@@ -56,7 +64,7 @@ module clk_gen(
       rx_clk <= 0;
     end
     else begin
-      if (rx_count < rx_max) begin
+      if (rx_count <= rx_max) begin
         rx_count <= rx_count + 1;
       end
       else begin
@@ -356,9 +364,10 @@ module uart_rx(
       recv_data: begin
         if (count < 15)
           count <= count + 1;
-        else
+        else begin
           count <= 0;
           bit_count <= bit_count + 1;
+        end
       end
       check_parity: begin
         if (count < 15)
